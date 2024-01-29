99d478248a27820a7eee19b1cb3f6901062ea958
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-01-30 06:15:49 +0900

    style <code>

--- next/app/components/CustomElementCode.tsx
+++ next/app/components/CustomElementCode.tsx
@@ -1,7 +1,9 @@
+import styles from "./CustomElementCode.module.css";
+
 interface Props {
   children: React.ReactNode;
 }
 
 export function CustomElementCode(props: Props) {
-  return <code>{props.children}</code>;
+  return <code className={styles.component}>{props.children}</code>;
 }
